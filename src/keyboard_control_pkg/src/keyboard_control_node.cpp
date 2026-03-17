0ddef20046990aa2218ddef872ff3bd7082c8ba3
k3ntacz <petrvass10@gmail.com>
2026-03-17 17:50:59 +0100

    Zaloha ulohy 4 - Keyboard Control Node

---
+++ src/keyboard_control_pkg/src/keyboard_control_node.cpp
@@ -0,0 +1,9 @@
+#include "rclcpp/rclcpp.hpp"
+#include "../include/KeyboardControl.hpp"
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<KeyboardControlNode>());
+    rclcpp::shutdown();
+    return 0;
+}
